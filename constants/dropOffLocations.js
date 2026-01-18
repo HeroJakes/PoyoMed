63d41fd284f0989b6b1a469d432d790466992a64
Ivan Lee <ivanlee2345@gmail.com>
2026-01-18 22:29:43 +0800

    feat: added drop off location

---
+++ constants/dropOffLocations.js
@@ -0,0 +1,95 @@
+export const DROP_OFF_LOCATIONS = [
+    {
+        id: 'kl-1',
+        name: 'Watsons Suria KLCC',
+        address: 'Lot C21K, Concourse Floor, Suria KLCC, 50088 Kuala Lumpur',
+        latitude: 3.1579,
+        longitude: 101.7123,
+        acceptedTypes: ['Tablets', 'Capsules', 'Syrups'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-2161 3111',
+        badge: 'Top Rated'
+    },
+    {
+        id: 'kl-2',
+        name: 'Guardian Mid Valley Megamall',
+        address: 'LG-035, Lower Ground Floor, Mid Valley Megamall, 59200 Kuala Lumpur',
+        latitude: 3.1186,
+        longitude: 101.6777,
+        acceptedTypes: ['Prescription Meds', 'OTC', 'Supplements'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-2284 3991',
+        badge: 'Large Capacity'
+    },
+    {
+        id: 'sel-1',
+        name: 'Caring Pharmacy Sunway Pyramid',
+        address: 'LG2, Sunway Pyramid Shopping Mall, 46150 Petaling Jaya, Selangor',
+        latitude: 3.0731,
+        longitude: 101.6075,
+        acceptedTypes: ['All Medications', 'Inhalers'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-5638 3188',
+        badge: 'Eco-Certified'
+    },
+    {
+        id: 'kl-3',
+        name: 'AA Pharmacy Bangsar Village',
+        address: 'Lot G12, Ground Floor, Bangsar Village, 1, Jalan Telawi 1, 59100 Kuala Lumpur',
+        latitude: 3.1301,
+        longitude: 101.6708,
+        acceptedTypes: ['Tablets', 'Capsules'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-2287 3688',
+        badge: 'Fast Drop-off'
+    },
+    {
+        id: 'sel-2',
+        name: 'Health Lane Family Pharmacy 1 Utama',
+        address: 'LG-313, Lower Ground Floor, 1 Utama Shopping Centre, 47800 Petaling Jaya, Selangor',
+        latitude: 3.1502,
+        longitude: 101.6155,
+        acceptedTypes: ['Liquids', 'Pills', 'Creams'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-7726 6388',
+        badge: 'Family Trust'
+    },
+    {
+        id: 'kl-4',
+        name: 'Aeon Wellness Pavilion KL',
+        address: 'Lot 1.02.00, Level 1, Pavilion Kuala Lumpur, 55100 Kuala Lumpur',
+        latitude: 3.1485,
+        longitude: 101.7131,
+        acceptedTypes: ['Premium Meds', 'OTC'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '03-2141 3111',
+        badge: 'Premium'
+    },
+    {
+        id: 'pen-1',
+        name: 'Guardian Gurney Plaza',
+        address: '170-B1-20/21, Gurney Plaza, Persiaran Gurney, 10250 Penang',
+        latitude: 5.4367,
+        longitude: 100.3086,
+        acceptedTypes: ['All Medications'],
+        hours: '10:00 AM - 10:00 PM',
+        phone: '04-226 3991',
+        badge: 'North Hub'
+    },
+    {
+        id: 'kl-5',
+        name: 'TESCO / Lotus Pharmacy Ampang',
+        address: 'Lot 1, Lotus Ampang, Jalan Pandan Prima, 55100 Kuala Lumpur',
+        latitude: 3.1401,
+        longitude: 101.7455,
+        acceptedTypes: ['Tablets', 'Capsules', 'OTC'],
+        hours: '8:00 AM - 10:00 PM',
+        phone: '03-9281 3111',
+        badge: 'Easy Access'
+    }
+];
+
+export const FALLBACK_USER_LOCATION = {
+    latitude: 3.1390, // Kuala Lumpur Center
+    longitude: 101.6869
+};
